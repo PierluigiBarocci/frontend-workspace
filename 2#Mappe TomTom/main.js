9b5b94235185eab88196ca2ce717488d6983c3b4
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-03-15 10:33:22 +0100

    Bozza Dettaglio stanza con mappa tomtom

---
+++ 2#Mappe TomTom/main.js
@@ -0,0 +1,391 @@
+// chiamata in GET
+// https://api.tomtom.com/search/2/geocode/via%20roma%2010%20jesi.json?countrySet=IT&key=fUMegNjIziihwACufGcHTnuftsDvb4NG
+
+// {
+//     "summary": {
+//         "query": "via roma 10 jesi",
+//         "queryType": "NON_NEAR",
+//         "queryTime": 38,
+//         "numResults": 10,
+//         "offset": 0,
+//         "totalResults": 43,
+//         "fuzzyLevel": 1
+//     },
+//     "results": [
+//         {
+//             "type": "Point Address",
+//             "id": "IT/PAD/p0/7270269",
+//             "score": 8.236200332641602,
+//             "address": {
+//                 "streetNumber": "10/A",
+//                 "streetName": "Via Roma",
+//                 "municipalitySubdivision": "Jesi",
+//                 "municipality": "Jesi",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60035",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma, 10/A, 60035 Jesi",
+//                 "localName": "Jesi"
+//             },
+//             "position": {
+//                 "lat": 43.51693,
+//                 "lon": 13.23517
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.51783,
+//                     "lon": 13.23393
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.51603,
+//                     "lon": 13.23641
+//                 }
+//             },
+//             "entryPoints": [
+//                 {
+//                     "type": "main",
+//                     "position": {
+//                         "lat": 43.51694,
+//                         "lon": 13.23514
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             "type": "Point Address",
+//             "id": "IT/PAD/p0/7413080",
+//             "score": 8.236200332641602,
+//             "address": {
+//                 "streetNumber": "10/B",
+//                 "streetName": "Via Roma",
+//                 "municipalitySubdivision": "Jesi",
+//                 "municipality": "Jesi",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60035",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma, 10/B, 60035 Jesi",
+//                 "localName": "Jesi"
+//             },
+//             "position": {
+//                 "lat": 43.51688,
+//                 "lon": 13.23515
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.51778,
+//                     "lon": 13.23391
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.51598,
+//                     "lon": 13.23639
+//                 }
+//             },
+//             "entryPoints": [
+//                 {
+//                     "type": "main",
+//                     "position": {
+//                         "lat": 43.51689,
+//                         "lon": 13.23512
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             "type": "Point Address",
+//             "id": "IT/PAD/p0/11670513",
+//             "score": 8.236200332641602,
+//             "address": {
+//                 "streetNumber": "10",
+//                 "streetName": "Via Roma",
+//                 "municipalitySubdivision": "Jesi",
+//                 "municipality": "Jesi",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60035",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma, 10, 60035 Jesi",
+//                 "localName": "Jesi"
+//             },
+//             "position": {
+//                 "lat": 43.5169,
+//                 "lon": 13.23516
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.5178,
+//                     "lon": 13.23392
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.516,
+//                     "lon": 13.2364
+//                 }
+//             },
+//             "entryPoints": [
+//                 {
+//                     "type": "main",
+//                     "position": {
+//                         "lat": 43.51691,
+//                         "lon": 13.23513
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             "type": "Point Address",
+//             "id": "IT/PAD/p0/245999",
+//             "score": 7.825860023498535,
+//             "address": {
+//                 "streetNumber": "10",
+//                 "streetName": "Via Jesi",
+//                 "municipalitySubdivision": "Roma",
+//                 "municipality": "Roma",
+//                 "countrySecondarySubdivision": "Roma",
+//                 "countrySubdivision": "Lazio",
+//                 "postalCode": "00156",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Jesi, 10, 00156 Roma",
+//                 "localName": "Roma"
+//             },
+//             "position": {
+//                 "lat": 41.94023,
+//                 "lon": 12.58082
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 41.94113,
+//                     "lon": 12.57961
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 41.93933,
+//                     "lon": 12.58203
+//                 }
+//             },
+//             "entryPoints": [
+//                 {
+//                     "type": "main",
+//                     "position": {
+//                         "lat": 41.94018,
+//                         "lon": 12.58073
+//                     }
+//                 }
+//             ]
+//         },
+//         {
+//             "type": "Street",
+//             "id": "IT/STR/p0/818294",
+//             "score": 6.452390193939209,
+//             "address": {
+//                 "streetName": "Via Roma",
+//                 "municipality": "Jesi",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60035",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma, 60035 Jesi",
+//                 "localName": "Jesi"
+//             },
+//             "position": {
+//                 "lat": 43.50629,
+//                 "lon": 13.19543
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.51522,
+//                     "lon": 13.18454
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.49724,
+//                     "lon": 13.20934
+//                 }
+//             }
+//         },
+//         {
+//             "type": "Street",
+//             "id": "IT/STR/p0/2263602",
+//             "score": 6.452390193939209,
+//             "address": {
+//                 "streetName": "Via Roma",
+//                 "municipalitySubdivision": "Jesi",
+//                 "municipality": "Jesi",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60035",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma, 60035 Jesi",
+//                 "localName": "Jesi"
+//             },
+//             "position": {
+//                 "lat": 43.5105,
+//                 "lon": 13.22785
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.52154,
+//                     "lon": 13.2146
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.50355,
+//                     "lon": 13.23941
+//                 }
+//             }
+//         },
+//         {
+//             "type": "Street",
+//             "id": "IT/STR/p0/463161",
+//             "score": 6.037300109863281,
+//             "address": {
+//                 "streetName": "Via Jesi",
+//                 "municipalitySubdivision": "Roma",
+//                 "municipality": "Roma",
+//                 "countrySecondarySubdivision": "Roma",
+//                 "countrySubdivision": "Lazio",
+//                 "postalCode": "00156",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Jesi, 00156 Roma",
+//                 "localName": "Roma"
+//             },
+//             "position": {
+//                 "lat": 41.94081,
+//                 "lon": 12.58014
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 41.94081,
+//                     "lon": 12.58014
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 41.93976,
+//                     "lon": 12.58112
+//                 }
+//             }
+//         },
+//         {
+//             "type": "Cross Street",
+//             "id": "IT/XSTR/p0/358392",
+//             "score": 5.661280155181885,
+//             "address": {
+//                 "streetName": "Via Jesi & Via Roma",
+//                 "municipalitySubdivision": "Cairate",
+//                 "municipality": "Cairate",
+//                 "countrySecondarySubdivision": "Varese",
+//                 "countrySubdivision": "Lombardia",
+//                 "postalCode": "21050",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Jesi & Via Roma, 21050 Cairate",
+//                 "localName": "Cairate"
+//             },
+//             "position": {
+//                 "lat": 45.69072,
+//                 "lon": 8.87251
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 45.69162,
+//                     "lon": 8.87122
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 45.68982,
+//                     "lon": 8.8738
+//                 }
+//             }
+//         },
+//         {
+//             "type": "Cross Street",
+//             "id": "IT/XSTR/p0/15045",
+//             "score": 5.644100189208984,
+//             "address": {
+//                 "streetName": "Via Roma, Strada Provinciale 4 & Via di Jesi, Strada Provinciale 4",
+//                 "municipalitySubdivision": "Santa Maria Nuova",
+//                 "municipality": "Santa Maria Nuova",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60030",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Roma & Via di Jesi, 60030 Santa Maria Nuova",
+//                 "localName": "Santa Maria Nuova"
+//             },
+//             "position": {
+//                 "lat": 43.4917,
+//                 "lon": 13.30184
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.4926,
+//                     "lon": 13.3006
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.4908,
+//                     "lon": 13.30308
+//                 }
+//             }
+//         },
+//         {
+//             "type": "Cross Street",
+//             "id": "IT/XSTR/p0/13348",
+//             "score": 5.321459770202637,
+//             "address": {
+//                 "streetName": "Via Ancona, Strada Provinciale 76 & Via Roma, Strada Provinciale 76",
+//                 "municipality": "Castelbellino",
+//                 "countrySecondarySubdivision": "Ancona",
+//                 "countrySubdivision": "Marche",
+//                 "postalCode": "60030",
+//                 "countryCode": "IT",
+//                 "country": "Italia",
+//                 "countryCodeISO3": "ITA",
+//                 "freeformAddress": "Via Ancona & Via Roma, 60030 Castelbellino",
+//                 "localName": "Castelbellino"
+//             },
+//             "position": {
+//                 "lat": 43.50636,
+//                 "lon": 13.1752
+//             },
+//             "viewport": {
+//                 "topLeftPoint": {
+//                     "lat": 43.50726,
+//                     "lon": 13.17396
+//                 },
+//                 "btmRightPoint": {
+//                     "lat": 43.50546,
+//                     "lon": 13.17644
+//                 }
+//             }
+//         }
+//     ]
+// }
+
+// una volta ottenute le coordinata
+// vanno inserite nel center di map
+const apiKey = 'fUMegNjIziihwACufGcHTnuftsDvb4NG';
+var coordinates = [13.23517, 43.51693];
+const map = tt.map({
+    key: apiKey,
+    container: 'map',
+    center: coordinates,
+    style: 'tomtom://vector/1/basic-main',
+    zoom: 15
+});
+// servono per creare il marker sulla mappa
+// e le funzioni di interazione
+var marker = new tt.Marker().setLngLat(coordinates).addTo(map);
+map.addControl(new tt.FullscreenControl());
+map.addControl(new tt.NavigationControl());
